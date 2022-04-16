3e79e364b0766bca69904b1e1b5adf61fe4c8cfa
JosephSaliba01 <josephsaliba54@gmail.com>
2022-04-16 02:57:39 -0400

    Set up for waveform reading and processing

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # import statements
 import sys
 import timeit
+from scipy.io.wavfile import read, write
 import matplotlib.pyplot as plt   # plotting 
 import numpy as np                # all of numpy...
-del sys.modules["numpy"].fft      # ... except FFT helpers
+# del sys.modules["numpy"].fft      # ... except FFT helpers
 
 def naive_DFT(inSignal, s: int = -1):
     """
@@ -53,7 +54,6 @@ def fast_DFT(inSignal, s: int = -1):
 
     This function uses the Cooley-Tukey algorithm.
     """
-    # TODO: Implement the fast DFT algorithm
     y = np.zeros(inSignal.shape, dtype = complex)
     N = inSignal.shape[0]
     if N < 32:
@@ -97,14 +97,6 @@ def plot_discrete(*signals, title=""):
     plt.show()
 
 
-def plot_continous(inSignal):
-    """
-    Plots the input signal.
-    """
-    plt.plot(inSignal)
-    plt.show()
-
-
 def discrete_sin(num_period, step):
     """
     Generates a sinusoid signal with the given amount of period and step.
@@ -117,9 +109,9 @@ def main():
     """
     Main method.
     """
-    y = discrete_sin(4, 0.125)
-    y_DFT = naive_DFT(y)
-    plot_discrete(y, y_DFT.real, y_DFT.imag, title="Naive plotting test")
+    # y = discrete_sin(4, 0.125)
+    # y_DFT = naive_DFT(y)
+    # plot_discrete(y, y_DFT.real, y_DFT.imag, title="Naive plotting test")
     
     ##benchmark FFTs performance against a naive DFT for increasingly large N
     DFTplot = np.array([])
@@ -132,14 +124,16 @@ def main():
         x = np.random.randn(N) + np.random.randn(N)*1j
 
         start_time = timeit.default_timer()
-        naive_DFT(x)
+        y = naive_DFT(x)
         DFTtime = timeit.default_timer() - start_time ##time it takes for DFT for a given N
         DFTplot = np.append(DFTplot, DFTtime)
+        assert np.allclose(y, np.fft.fft(x)) # assert correctness of naive DFT
 
         start_time = timeit.default_timer()
-        fast_DFT(x)
+        y = fast_DFT(x)
         FFTtime = timeit.default_timer() - start_time ##time it takes for FFT for a given N
         FFTplot = np.append(FFTplot, FFTtime)
+        assert np.allclose(y, np.fft.fft(x)) # assert correctness of fast DFT
 
     plt.plot(Naxis,DFTplot)
     plt.plot(Naxis,FFTplot)
@@ -149,6 +143,30 @@ def main():
     plt.title('Performance of DFT/FFT as a function of N')
     plt.show()
 
+    # Benchmark using Falcon voice sample
+
+    waveform = read("audio/Falcon-sound-2.wav")[1].T[0]  # read in the audio file
+    
+    start_time = timeit.default_timer()
+    y = naive_DFT(waveform)
+    DFTtime = timeit.default_timer() - start_time
+    plot_discrete(waveform, y.real, y.imag, title="Falcon voice sample using naive DFT")
+
+    start_time = timeit.default_timer()
+    y = fast_DFT(waveform)
+    FFTtime = timeit.default_timer() - start_time
+    plot_discrete(waveform, y.real, y.imag, title="Falcon voice sample using fast DFT")
+
+    start_time = timeit.default_timer()
+    y = np.fft.fft(waveform)
+    npFFTtime = timeit.default_timer() - start_time
+    plot_discrete(waveform, y.real, y.imag, title="Falcon voice sample using numpy.fft.fft")
+
+    print("Naive DFT: ", DFTtime)
+    print("Fast DFT: ", FFTtime)
+    print("Numpy FFT: ", npFFTtime)
+
+    assert np.allclose(fast_DFT(waveform), np.fft.fft(waveform))
 
 if __name__ == "__main__":
     main()
